c7e3e271c0a8f633b90987222a0902a50f10e4db
Tynar Esenbaeva <esenbaevat@yahoo.com>
2023-03-16 09:46:09 -0400

    update

--- iam.tf
+++ iam.tf
@@ -42,3 +42,49 @@ resource "aws_iam_role_policy_attachment" "s3_backend_policy_attachment" {
   policy_arn = aws_iam_policy.s3_backend_policy.arn
   role       = aws_iam_role.s3_backend_role.name
 }
+
+resource "aws_iam_policy" "s3_backend_s3_policy" {
+  name        = "s3-backend-s3-policy"
+  description = "Policy for S3 backend to access S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "ListBucketObjects"
+        Effect   = "Allow"
+        Action   = [
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::your-bucket-name"
+        ]
+      },
+      {
+        Sid      = "GetObject"
+        Effect   = "Allow"
+        Action   = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::your-bucket-name/*"
+        ]
+      },
+      {
+        Sid      = "PutObject"
+        Effect   = "Allow"
+        Action   = [
+          "s3:PutObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::your-bucket-name/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_backend_s3_policy_attachment" {
+  policy_arn = aws_iam_policy.s3_backend_s3_policy.arn
+  role       = aws_iam_role.s3_backend_role.name
+}
